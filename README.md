68fb58655db698ff664d52b393f0f5834a79cbf0
Samuel David De La Cruz <alphaelena@gmail.com>
2018-01-22 17:52:10 -0400

    version bump and update readme

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # ASYNC_JSON
 Promisified JSON.parse and JSON.stringify for non blocking parsing of json using napa.js
+## Installation
+Install the latest stable version:
+```
+npm i @samuel023/async_json
+```
+## Quick Start
+```js
+//promisified/async JSON.stringify
+const AsyncJSON = require('@samuel023/async_json');
+
+const {stringifyAsync} = AsyncJSON();
+
+const obj = {name:"john",lastname:"doe"}
+stringifyAsync(obj).then( result => { 
+    console.log(result); //{"name":"john","lastname":"doe"} 
+ });
+
+console.log(await stringifyAsync(obj));//{"name":"john","lastname":"doe"} 
+ 
+```
+
+```js
+//promisified/async JSON.parse
+const AsyncJSON = require('@samuel023/async_json');
+
+const {parseAsync} = AsyncJSON();
+
+const jsonstr = `{"name":"john","lastname":"doe"}`
+parseAsync(jsonstr).then( result => { 
+    console.log(result); //{ name: 'john', lastname: 'doe' }
+ });
+
+console.log(await parseAsync(obj));//{ name: 'john', lastname: 'doe' } 
+ 
+```
--- test/index.test.js
+++ test/index.test.js
@@ -6,62 +6,63 @@ chai.use(chaiAsPromised);
 const should = chai.should();
 const {
     stringifyAsync,
-    parseAsync} = AsyncJSON();
-
-
-    const testJson = {
-        "glossary": {
-            "title": "example glossary",
-            "GlossDiv": {
-                "title": "S",
-                "GlossList": {
-                    "GlossEntry": {
-                        "ID": "SGML",
-                        "SortAs": "SGML",
-                        "GlossTerm": "Standard Generalized Markup Language",
-                        "Acronym": "SGML",
-                        "Abbrev": "ISO 8879:1986",
-                        "GlossDef": {
-                            "para": "A meta-markup language, used to create markup languages such as DocBook.",
-                            "GlossSeeAlso": [
-                                "GML",
-                                "XML"
-                            ]
-                        },
-                        "GlossSee": "markup"
-                    }
+    parseAsync } = AsyncJSON();
+  
+    
+const testJson = {
+    "glossary": {
+        "title": "example glossary",
+        "GlossDiv": {
+            "title": "S",
+            "GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+                    "SortAs": "SGML",
+                    "GlossTerm": "Standard Generalized Markup Language",
+                    "Acronym": "SGML",
+                    "Abbrev": "ISO 8879:1986",
+                    "GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+                        "GlossSeeAlso": [
+                            "GML",
+                            "XML"
+                        ]
+                    },
+                    "GlossSee": "markup"
                 }
             }
         }
-    }    
+    }
+}
 
-describe('stringifyAsync',()=>{
+describe('stringifyAsync', () => {
 
-    it('should return same string as JSON.stringify',(done) => {
+    it('should return same string as JSON.stringify', (done) => {
         const strJson = JSON.stringify(testJson);
 
         stringifyAsync(testJson).should.eventually.equal(strJson).notify(done);
-       
+
     })
 });
 
-describe('parseAsync',()=>{
+describe('parseAsync', () => {
 
-    it('should return same object as JSON.parse',(done) => {
+    it('should return same object as JSON.parse', (done) => {
         const strJson = JSON.stringify(testJson);
-
+   
+       
         const parsedJson = JSON.parse(strJson);
 
         parseAsync(strJson).should.eventually.deep.equal(parsedJson).notify(done);
-       
+
     });
 
-    it('should reject on invalid string/input',(done) => {
+    it('should reject on invalid string/input', (done) => {
         const strJson = JSON.stringify(testJson);
 
         const parsedJson = JSON.parse(strJson);
 
         parseAsync(undefined).should.be.rejected.notify(done);
-       
+
     });
 });
